3c8e8dda2c407d7c50c3ff8943d5479a002a15db
marcelo.vergara <marcelorv@gmail.com>
2026-05-03 15:39:20 -0300

    conf: Update CORS configuration and add production environment settings

--- api/src/main/java/com/vergaraverse/api/config/WebConfig.java
+++ api/src/main/java/com/vergaraverse/api/config/WebConfig.java
@@ -10,7 +10,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins(
+                        "http://localhost:4200", 
+                        "https://vergaraverse.web.app", 
+                        "https://vergaraverse.firebaseapp.com"
+                )
                 .allowedMethods("GET", "POST", "OPTIONS")
                 .allowedHeaders("Content-Type", "Accept");
     }
